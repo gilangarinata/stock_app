67fab54d04aaa747d859b7115d2d962ce5c5aa34
gilangarinata <gilangarinata@gmail.com>
2020-02-13 14:29:37 +0700

    -change login

--- application/views/admin/login.php
+++ application/views/admin/login.php
@@ -6,7 +6,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <title>ADMIN | Login</title>
+    <title>ADMINSSS | Login</title>
 
     <link href="<?= base_url() ?>assets/css/bootstrap.min.css" rel="stylesheet">
     <link href="<?= base_url() ?>assets/font-awesome/css/font-awesome.css" rel="stylesheet">
